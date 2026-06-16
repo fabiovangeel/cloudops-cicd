118f14c08cf5421683ca7f3bc93eaf7b150fd1d2
Fabio Vangeel <fabio.vangeel@student.ucll.be>
2026-06-16 20:27:03 +0200

    Initial setup: app, k8s manifests, CI/CD workflows

---
+++ app/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('CloudOps CI/CD - v1.0'));
+app.listen(3000, () => console.log('App running on 3000'));
+module.exports = app;
